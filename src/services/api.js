b5ca916e5c533f7d6b7b88e76d8feb93dccdb7d6
João Lírio <joaovictorjgar@gmail.com>
2025-01-26 17:36:05 -0300

    Deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 
+// const api = axios.create({
+//     baseURL: 'http://localhost:3000'
+// })
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: "https://apicardpoints.onrender.com"
 })
 
 export default api
